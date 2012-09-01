958bdcc8d79cbd2a54964a1a8d2c6205aa4206df
McLuke300 <lcherry1997@gmail.com>
2012-09-01 16:19:53 +0100

    Version 0.3

--- src/me/mcluke300/ipadvlog/ipadvlog.java
+++ src/me/mcluke300/ipadvlog/ipadvlog.java
@@ -14,6 +14,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -30,11 +31,17 @@ public class ipadvlog  extends JavaPlugin implements Listener{
 	File subdir = new File("plugins/AdvIpLog/");
 
 	public static ipadvlog plugin;
+	
 	@Override
 	public void onEnable() {
 		plugin = this;
+		LoadConfiguration();
+
+		//The Ymls
 		PlayerFile = new File(getDataFolder(), "players.yml");
 		IpFile = new File(getDataFolder(), "ips.yml");
+
+		//Creating the files
 		if (!subdir.exists()) {
 			subdir.mkdir();
 		}
@@ -44,29 +51,44 @@ public class ipadvlog  extends JavaPlugin implements Listener{
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-		}
+			}}
 		if (!IpFile.exists()) {
 			try {
 				IpFile.createNewFile();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-		}
+			}}
+
+		//Yaml configuration
 		Players = new YamlConfiguration();
 		Ips = new YamlConfiguration();
 		loadYamls();
+
+		//Metrics
 		try {
 			MetricsLite metrics = new MetricsLite(this);
 			metrics.start();
 		} catch (IOException e) {
 			// Failed to submit the stats :-(
 		}
+
+		//Registering Events
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
 
+	//Config
+	public void LoadConfiguration() {
+		String path = "IpAdvLog.UseAuthCheck";
+		String path2 = "IpAdvLog.Alert.AlertAdmins";
+		String path3 = "IpAdvLog.Alert.AlertMessage";
+		getConfig().addDefault(path, true);
+		getConfig().addDefault(path2, true);
+		getConfig().addDefault(path3, "[&cAIL&f]&2%name% &aAlt accounts %alts%");
+		getConfig().options().copyDefaults(true);
+		saveConfig();
 
+	}
 
 	//Saving The Yamls
 	public void saveYamls() {
@@ -91,7 +113,7 @@ public class ipadvlog  extends JavaPlugin implements Listener{
 
 
 
-
+	//On Disable
 	@Override
 	public void onDisable() {
 		list.clear();
@@ -99,114 +121,129 @@ public class ipadvlog  extends JavaPlugin implements Listener{
 
 
 
-
-
 	//Join Event
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerEvent(PlayerJoinEvent e) {
+		//Adding to list
 		Player p = e.getPlayer();
 		String playername = p.getName();
+		if(plugin.getConfig().getBoolean("IpAdvLog.UseAuthCheck")) {
 		list.add(playername);
-		List<String> alts = new ArrayList<String>();
-		if (Players.getStringList(playername.toLowerCase()).isEmpty()) {
 		} else {
-			for (String b : Players.getStringList(playername.toLowerCase())) {
-				b = b.replaceAll("\\.", "_");
-				for (String c : Ips.getStringList(b)) {
-					if (!alts.contains(c)) {
-					alts.add(c);
+			addYaml(playername, p.getAddress().getAddress().getHostAddress());	
+		}
+		
+		//Alert on alt accounts
+		if (plugin.getConfig().getBoolean("IpAdvLog.Alert.AlertAdmins")  && !p.hasPermission("ail.ignorealert")) {
+			List<String> alts = new ArrayList<String>();
+			if (Players.getStringList(playername.toLowerCase()).isEmpty()) {
+			} else {
+				for (String b : Players.getStringList(playername.toLowerCase())) {
+					b = b.replaceAll("\\.", "_");
+					for (String c : Ips.getStringList(b)) {
+						if (!alts.contains(c)) {
+							alts.add(c);
+						}
+					}
+				}
+			}
+			if (alts.size() > 1) {
+				String msg = plugin.getConfig().getString("IpAdvLog.Alert.AlertMessage");
+				msg = msg.replaceAll("%name%", playername);
+				msg = msg.replaceAll("%alts%", alts.toString());
+				msg = msg.replaceAll("&([0-9a-fA-F])", "§$1");
+				for (Player c : Bukkit.getServer().getOnlinePlayers()) {
+					if (c.hasPermission("ail.alert")) {
+						c.sendMessage("["+ChatColor.RED+"AIL"+ChatColor.WHITE+"]"+ChatColor.DARK_GREEN+playername+ChatColor.GREEN+" Alt Accounts " + alts);
 					}
 				}
 			}
 		}
-		System.out.print(alts);
-		if (alts.size() > 1) {
-			for (Player c : Bukkit.getServer().getOnlinePlayers()) {
-				if (c.hasPermission("ail.alert")) {
-					c.sendMessage("["+ChatColor.RED+"AIL"+ChatColor.WHITE+"]"+ChatColor.DARK_GREEN+playername+ChatColor.GREEN+" Alt Accounts " + alts);
-				}
-			}
-		}
-		
+
 	}
 
-	
-	
+
+
 
 	//On Move event
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerMove(PlayerMoveEvent e) {
+		if(plugin.getConfig().getBoolean("IpAdvLog.UseAuthCheck")) {
 		if (list.contains(e.getPlayer().getName())) {
+			list.remove(e.getPlayer().getName());
 			if (e.isCancelled() == false) {
 				Player p = e.getPlayer();
 				String playername = p.getName();
 				playername = playername.toLowerCase();
 				String ip = p.getAddress().getAddress().getHostAddress();
 				list.remove(playername);
-				loadYamls();
-
-
-				//Players.yml
-				if (Players.getStringList(playername) == null) {
-					List<String> list = new ArrayList<String>();
-					list.add(ip);
-					Players.addDefault(playername, list);
-				} else {
-					List<String> list2 = Players.getStringList(playername);
-					if (!list2.contains(ip)) {
-						list2.add(ip);
-						Players.set(playername, list2);
-					}
-
-				}
-				try {
-					Players.save(PlayerFile);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-
-
-
-				//Ips.yml
-				ip = ip.replaceAll("\\.", "_");
-				if (Ips.getStringList(ip) == null) {
-					List<String> list3 = new ArrayList<String>();
-					list3.add(playername);
-					Ips.addDefault(ip, list3);
-				} else {
-					List<String> list4 = Ips.getStringList(ip);
-					Boolean doesit = false;
-					for (String b : list4) {
-						if (b.equalsIgnoreCase(playername)) {
-							doesit = true;
-						}}
-					if (!doesit) {
-						list4.add(playername);
-						Ips.set(ip, list4);
-					}
-					try {
-						Ips.save(IpFile);
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}	
+				addYaml(playername, ip);
 				}}}}
+	
+	
+	//Adding and removing from yaml
+	public void addYaml(String playername, String ip) {
+		if (Players.getStringList(playername) == null) {
+			List<String> list = new ArrayList<String>();
+			list.add(ip);
+			Players.addDefault(playername, list);
+		} else {
+			List<String> list2 = Players.getStringList(playername);
+			if (!list2.contains(ip)) {
+				list2.add(ip);
+				Players.set(playername, list2);
+			}
+
+		}
+		try {
+			Players.save(PlayerFile);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+
+
+		//Ips.yml
+		ip = ip.replaceAll("\\.", "_");
+		if (Ips.getStringList(ip) == null) {
+			List<String> list3 = new ArrayList<String>();
+			list3.add(playername);
+			Ips.addDefault(ip, list3);
+		} else {
+			List<String> list4 = Ips.getStringList(ip);
+			Boolean doesit = false;
+			for (String b : list4) {
+				if (b.equalsIgnoreCase(playername)) {
+					doesit = true;
+				}}
+			if (!doesit) {
+				list4.add(playername);
+				Ips.set(ip, list4);
+			}
+			try {
+				Ips.save(IpFile);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}}}
+
+
 
 	//Join Event
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerQuit(PlayerQuitEvent e) {
 		Player p = e.getPlayer();
 		String playername = p.getName();
 		if (list.contains(playername)) {
-		list.remove(playername);
-	}}
+			list.remove(playername);
+		}}
 
 
 
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		if ((commandLabel.equalsIgnoreCase("ail"))) {
+		if (commandLabel.equalsIgnoreCase("ail")) {
 
 			//Command Guide
 			if (args.length < 1) {
@@ -214,6 +251,7 @@ public class ipadvlog  extends JavaPlugin implements Listener{
 				sender.sendMessage(ChatColor.DARK_GREEN+"/ail player <PlayerName>"+ChatColor.BLUE+"  Lists all the ips the player uses");
 				sender.sendMessage(ChatColor.DARK_GREEN+"/ail ip <ip>"+ChatColor.BLUE+"  Lists all players using that ip");
 				sender.sendMessage(ChatColor.DARK_GREEN+"/ail check <PlayerName>"+ChatColor.BLUE+"  Will find all players alternate accounts");
+				sender.sendMessage(ChatColor.DARK_GREEN+"/ail reload"+ChatColor.BLUE+"  Will reload the plugin");
 			} else {
 
 				//Usage If only 1 Arg
@@ -225,70 +263,86 @@ public class ipadvlog  extends JavaPlugin implements Listener{
 							if (args[0].equalsIgnoreCase("ip")) {
 								if (sender.hasPermission("ail.ip")) {
 									sender.sendMessage(ChatColor.RED+"USAGE: "+ChatColor.GREEN+"/ail ip <ip>");
-								}}}} else if (args[0].equalsIgnoreCase("check")) {
+								}} else if (args[0].equalsIgnoreCase("check")) {
 									if (sender.hasPermission("ail.ip")) {
 										sender.sendMessage(ChatColor.RED+"USAGE: "+ChatColor.GREEN+"/ail check <PlayerName>");
-									}}{
+									}}else if (args[0].equalsIgnoreCase("reload")) {
+										if (sender.hasPermission("ail.reload")) {
+											getConfig();
+											reloadConfig();
+											getServer().getPluginManager().disablePlugin(plugin);
+											getServer().getPluginManager().enablePlugin(plugin);
+											sender.sendMessage(ChatColor.GREEN+"AdvIpLog Reloaded");
+										}}}}
 
-									//Two Args
-									if (args.length == 2) {
+										//Two Args
+										if (args.length == 2) {
 
 
-										//player command
-										if (args[0].equalsIgnoreCase("player")) {
-											if (sender.hasPermission("ail.player")) {
-												if (Players.getStringList(args[1].toLowerCase()) != null) {
-													if (Players.getStringList(args[1].toLowerCase()).isEmpty()) {
-														sender.sendMessage(ChatColor.RED+"Cannot find player "+args[1]);
-													} else {
-														sender.sendMessage(ChatColor.GREEN+"Ip Addresses used by "+ChatColor.BLUE+args[1]);
-														for (String a : Players.getStringList(args[1].toLowerCase())) {
-															sender.sendMessage(ChatColor.DARK_GREEN+"- "+a);
-														}}}}}else {
-															
-															//Ip Command
-															if (args[0].equalsIgnoreCase("ip")) {
-																if (sender.hasPermission("ail.ip")) {
-																	if (Ips.getStringList(args[1].replaceAll("\\.", "_")) != null) {
-																		if (Ips.getStringList(args[1].replaceAll("\\.", "_")).isEmpty()) {
-																			sender.sendMessage(ChatColor.RED+"Cannot find Ip "+args[1]);
-																		} else {
-																			sender.sendMessage(ChatColor.GREEN+"Players with Ip "+ChatColor.BLUE+args[1]);
-																			for (String a : Ips.getStringList(args[1].replaceAll("\\.", "_"))) {
-																				sender.sendMessage(ChatColor.DARK_GREEN+"- "+a);
-																				
-																				
-																				
-																				//Check Command
-																			}}}}} else if (args[0].equalsIgnoreCase("check")) {
-																				if (sender.hasPermission("ail.check")) {
-																					List<String> checker = new ArrayList<String>();
-																					if (Players.getStringList(args[1].toLowerCase()).isEmpty()) {
-																						sender.sendMessage(ChatColor.RED+"Cannot find player "+args[1]);
-																					} else {
-																						for (String b : Players.getStringList(args[1].toLowerCase())) {
-																							b = b.replaceAll("\\.", "_");
-																							for (String c : Ips.getStringList(b)) {
-																								if (!checker.contains(c)) {
-																								checker.add(c);
+											//player command
+											if (args[0].equalsIgnoreCase("player")) {
+												if (sender.hasPermission("ail.player")) {
+													if (Players.getStringList(args[1].toLowerCase()) != null) {
+														if (Players.getStringList(args[1].toLowerCase()).isEmpty()) {
+															sender.sendMessage(ChatColor.RED+"Cannot find player "+args[1]);
+														} else {
+															sender.sendMessage(ChatColor.GREEN+"Ip Addresses used by "+ChatColor.BLUE+args[1]);
+															for (String a : Players.getStringList(args[1].toLowerCase())) {
+																List<String> checker = new ArrayList<String>();
+																a = a.replaceAll("\\.", "_");
+																for (String c : Ips.getStringList(a)) {
+																	if (!checker.contains(c)) {
+																		checker.add(c);
+																	}
+																}
+																String alts = "";
+																if (checker.size() > 1) {
+																	alts = "[" +checker.size() + " Users]";
+																} else {
+																	alts = "";
+																}
+
+																a = a.replaceAll("\\_", ".");
+																sender.sendMessage(ChatColor.DARK_GREEN+"- "+a+"  "+ChatColor.RED +alts);
+															}}}}}else {
+
+																//Ip Command
+																if (args[0].equalsIgnoreCase("ip")) {
+																	if (sender.hasPermission("ail.ip")) {
+																		if (Ips.getStringList(args[1].replaceAll("\\.", "_")) != null) {
+																			if (Ips.getStringList(args[1].replaceAll("\\.", "_")).isEmpty()) {
+																				sender.sendMessage(ChatColor.RED+"Cannot find Ip "+args[1]);
+																			} else {
+																				sender.sendMessage(ChatColor.GREEN+"Players with Ip "+ChatColor.BLUE+args[1]);
+																				for (String a : Ips.getStringList(args[1].replaceAll("\\.", "_"))) {
+																					sender.sendMessage(ChatColor.DARK_GREEN+"- "+a);
+
+
+
+																					//Check Command
+																				}}}}} else if (args[0].equalsIgnoreCase("check")) {
+																					if (sender.hasPermission("ail.check")) {
+																						List<String> checker = new ArrayList<String>();
+																						if (Players.getStringList(args[1].toLowerCase()).isEmpty()) {
+																							sender.sendMessage(ChatColor.RED+"Cannot find player "+args[1]);
+																						} else {
+																							for (String b : Players.getStringList(args[1].toLowerCase())) {
+																								b = b.replaceAll("\\.", "_");
+																								for (String c : Ips.getStringList(b)) {
+																									if (!checker.contains(c)) {
+																										checker.add(c);
+																									}
 																								}
 																							}
-																						}
-																						sender.sendMessage(ChatColor.GREEN+"Other accounts from user  "+ChatColor.BLUE+args[1]);
-																						for (String d : checker) {
-																							sender.sendMessage(ChatColor.DARK_GREEN+"- "+d );
-																						}
-																						
-																					}
-																				}
-																			}
-															
-														}}}}
-				return false;
+																							sender.sendMessage(ChatColor.GREEN+"Other accounts from user  "+ChatColor.BLUE+args[1]);
+																							for (String d : checker) {
+																								sender.sendMessage(ChatColor.DARK_GREEN+"- "+d );
+																							}
 
-			}
-		return false;
-	}
+															}}}}}}}
+			return false;
+
+		}
 }
 
 
